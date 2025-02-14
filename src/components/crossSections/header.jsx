31cb45dd5fca815294da14b9ed6fd4de5599f0fb
Cintya Olivares <cint@Cintyas-Mac-mini.local>
2025-02-14 19:11:07 -0300

    union header logos y footer

--- src/components/crossSections/header.jsx
+++ src/components/crossSections/header.jsx
@@ -10,44 +10,10 @@ const Header = () => {
 
     return (
 
-        <header className="bg-gray-200 p-4 flex justify-between items-center">
-            <a href="/" className="flex items-center">
+        <header className='sticky top-0 bg-white flex flex-row p-4 justify-between items-center shadow-md'>
+        <a href="/" className="flex items-center">
                 <img src="/src/assets/logo-light.png" alt="logo" className="h-12" />
             </a>
-            <div>
-                {user ? (
-                    // Usuario autenticado
-                    <div className="flex items-center gap-4">
-                        <span className="text-gray-700">
-                            Bienvenido, {user.username}
-                        </span>
-                        <button 
-                            onClick={handleLogout}
-                            className="bg-golden hover:bg-golden-light text-white font-bold py-2 px-4 rounded"
-                        >
-                            Cerrar Sesión
-                        </button>
-                    </div>
-                ) : (
-                    // Usuario no autenticado
-                    <div className="flex gap-4">
-                        <button 
-                            onClick={() => handleAuth('register')}
-                            className="bg-golden hover:bg-golden-light text-white font-bold py-2 px-4 rounded"
-                        >
-                            Crear Cuenta
-                        </button>
-                        <button 
-                            onClick={() => handleAuth('login')}
-                            className="border-2 border-golden text-golden hover:bg-golden hover:text-white font-bold py-2 px-4 rounded transition-colors"
-                        >
-                            Iniciar Sesión
-                        </button>
-                    </div>
-                )}
-
-        <header className='sticky top-0 bg-white flex flex-row p-4 justify-between items-center shadow-md'>
-            <img className="" src="src/assets/alquitonesLogo.png" alt="logo" />
             <div className='hidden md:block md:w-auto p-1 text-sm font-normal text-gray-600'>
                 <nav className="flex items-center space-x-4 mr-2">
                     <a className='relative block rounded-lg p-2 transition hover:scale-125' href="/">Inicio</a>
