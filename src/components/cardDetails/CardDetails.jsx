487b7f36eb5dd6193c7fcccaaea3290b8af13e0a
Math92 <mathiassan92@gmail.com>
2025-02-21 11:13:45 -0300

    Changing Back Span to Arrow Icon in Products View

--- src/components/cardDetails/CardDetails.jsx
+++ src/components/cardDetails/CardDetails.jsx
@@ -51,8 +51,9 @@ function CardDetails() {
         <div className="max-w-6xl mx-auto p-4 md:p-8 bg-gray-100">
             <div className="flex justify-between items-center mb-6">
                 <h1 className="text-2xl font-bold text-(--color-secondary)">{instrument.name} <span className="text-gray-500 text-sm">cod {instrument.id}</span></h1>
-                <button onClick={() => navigate(-1)} className="text-gray-600 hover:text-(--color-secondary) cursor-pointer">Atrás</button>
-            </div>
+                <button onClick={() => navigate(-1)} className="text-gray-600 hover:text-(--color-secondary) cursor-pointer text-2xl">
+    <span className="material-symbols-outlined">arrow_back</span>
+</button>            </div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 <img src={instrument.images[0]} alt={instrument.name} className="w-full h-96 object-cover rounded-lg" />
                 <div className="grid grid-cols-2 gap-2 md:col-span-2">
