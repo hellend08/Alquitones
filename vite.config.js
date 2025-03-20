2ac3caea965f64b5d89af1543cd684e943caf04d
Eduardo González <elduz.g@gmail.com>
2025-03-20 15:36:05 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,15 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
-})
+  server: {
+    host: '0.0.0.0',  // Expone la app públicamente
+    port: 5173, // Usa el puerto asignado por Railway
+    strictPort: true
+  },
+  preview: {
+    host: '0.0.0.0',  // Expone el preview públicamente
+    port:  4173,
+    strictPort: true,
+    allowedHosts: ["alquitonesfront-production.up.railway.app"]
+  }
+});
